eeb4672ab7f5855c49c6a40a7a8c963a7de34306
Monique da Silva <moniquedasilva2005@gmail.com>
2025-05-11 18:24:28 -0300

    questão feita pelo professor

---
+++ aulaPratica/aposta/PrincipalSorteGrande.java
@@ -0,0 +1,28 @@
+package aulaPratica.aposta;
+
+import java.util.Scanner;
+
+public class PrincipalSorteGrande {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Quantas apostas você quer fazer?");
+        int quantidadeApostas = teclado.nextInt();
+        SorteGrande sorteGrande = new SorteGrande(quantidadeApostas);
+
+        for(int i = 0; i < quantidadeApostas; i++){
+            System.out.printf("Digite a quantidade de números da %dª aposta \n", i + 1);
+            int qtdeNumeros = teclado.nextInt();
+            int[] aposta = new int[qtdeNumeros];
+            for(int j = 0; j < aposta.length; j++){
+                System.out.printf("Digite o %dº número da %dª aposta \n", j + 1, i + 1);
+                aposta[j] = teclado.nextInt();
+            }
+
+            sorteGrande.adicionaAposta(aposta);
+        }
+
+        sorteGrande.imprimirApostas();
+
+        teclado.close();
+    }
+}
---
+++ aulaPratica/aposta/SorteGrande.java
@@ -0,0 +1,33 @@
+package aulaPratica.aposta;
+
+public class SorteGrande {
+     private int[][] apostas;
+    private int indice;
+
+    public SorteGrande(int quantidadeApostas){
+        indice = 0;
+        apostas = new int[quantidadeApostas][];
+    }
+
+    public void adicionaAposta(int[] aposta){
+        apostas[indice++] = aposta;
+    }
+
+    public void imprimirApostas(){
+        for(int linha = 0; linha < apostas.length; linha++){
+            for(int coluna = 0; coluna < apostas[linha].length; coluna++){
+                System.out.printf("%d ", apostas[linha][coluna]);
+            }
+            System.out.println();
+        }
+    }
+
+    public void imprimirResumido(){
+        for(int[] coluna : apostas){
+            for(int numero : coluna){
+                System.out.printf("%d ", numero);
+            }
+            System.out.println();
+        }
+    }
+}
